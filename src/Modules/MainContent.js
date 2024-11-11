c39375155744016bea82870a2506c6c89ac69bcb
kvdcre8r <kvdcre8r@gmail.com>
2024-11-11 15:03:59 -0600

    testing run build

--- src/Modules/MainContent.js
+++ src/Modules/MainContent.js
@@ -243,8 +243,8 @@ function saveAsPDF() {
 
   html2canvas(content).then((canvas) => {
       const imgData = canvas.toDataURL("image/png");
-      const imgWidth = 210; // A4 width in mm
-      const pageHeight = 297; // A4 height in mm
+      const imgWidth = 210;
+      const pageHeight = 297;
       const imgHeight = (canvas.height * imgWidth) / canvas.width;
 
       let heightLeft = imgHeight;
